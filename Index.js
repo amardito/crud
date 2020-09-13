9d94c056906d6bf5c8ef0efef397fb637e3780c7
Reza Andriansyah <technoboth@gmail.com>
2020-09-13 09:33:08 +0700

    create tools for compression data

--- Index.js
+++ Index.js
@@ -1,14 +1,10 @@
 import { setup } from "./lib/mongo/index.js";
 import { getCount, find, findOne, findRange, findPage } from "./lib/mongo/find.js";
+import { unzip } from './lib/compress/index.js'
 
 const init = async () => {
-
   setup("mongodb://127.0.0.1:27017/", "testing", "room"); 
-
-  let arr = await findOne();
-
-  console.log(arr);
-
+  await find();    
 }
 
 init();
