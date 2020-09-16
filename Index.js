1e06a33ab058343789558a5481d88a42cf44fbf3
Reza Andriansyah <technoboth@gmail.com>
2020-09-17 00:44:13 +0700

    add express for testing speed and compare size data from array to buffer

--- Index.js
+++ Index.js
@@ -1,23 +1,34 @@
 import { getCount, find, findOne, findRange, findPage } from "./lib/mongo/find.js";
-import { gzip } from "./lib/compress/index.js";
+import { gzip, unzip } from "./lib/compress/index.js";
+
 import Mongo from "./lib/mongo/index.js";
+import compression from 'compression';
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+app.use(compression());
 
 const mongo = new Mongo();
 const mongo1 = new Mongo();
 
-const init = async () => {
-  
-  const obj1 = mongo.setup("mongodb://127.0.0.1:27017", "testing", "room");
-  const obj2 = mongo1.setup("mongodb://127.0.0.1:27017", "testing", "user");
+const obj1 = mongo.setup("mongodb://127.0.0.1:27017", "testing", "room");
+const obj2 = mongo1.setup("mongodb://127.0.0.1:27017", "testing", "user");
 
-  let [res, res2] = await Promise.all([find(obj1), find(obj2)]);  
-  
-  console.log(gzip(res.toString()));  
-  console.log(gzip(res2.toString()));  
+app.get('/', async (req,res)=>{
 
-  //console.log(res);
-  //console.log(res2);
+  res.setHeader('content-type', 'application/json');
 
-}
+  let [result, result2] = await Promise.all([find(obj1), find(obj2)]);  
 
-init();
+  //let zip1 = gzip(result);
+  let zip2 = gzip(result2);
+
+  //let unzip1 = unzip(zip1);
+  //let unzip2 = unzip(zip2);
+
+  res.send(result2).status(200);
+
+});
+
+app.listen(4000);
