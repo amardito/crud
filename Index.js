4dfd362275ac60345c510b2ca79914479fe578fc
Reza Andriansyah <technoboth@gmail.com>
2020-09-17 01:00:09 +0700

    fix bug at page data buffer

--- Index.js
+++ Index.js
@@ -25,9 +25,9 @@ app.get('/', async (req,res)=>{
   let zip2 = gzip(result2);
 
   //let unzip1 = unzip(zip1);
-  //let unzip2 = unzip(zip2);
+  let unzip2 = unzip(zip2, 100, 2);
 
-  res.send(result2).status(200);
+  res.send(unzip2).status(200);
 
 });
 
